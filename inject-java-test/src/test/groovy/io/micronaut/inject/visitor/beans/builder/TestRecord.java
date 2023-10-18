f2bc0c0f36b273e153df071c156e9475d983eb89
dawnzs01 <18013865675@163.com>
2023-10-18 18:38:51 +0800

    byte-buddy-fix

--- inject-java-test/src/test/groovy/io/micronaut/inject/visitor/beans/builder/TestRecord.java
+++
@@ -1,7 +0,0 @@
-package io.micronaut.inject.visitor.beans.builder;
-
-import io.micronaut.core.annotation.Introspected;
-
-@Introspected
-public record TestRecord(String name, int age) {
-}
--- test-suite/src/test/java/io/micronaut/docs/ioc/mappers/ContactForm.java
+++
@@ -1,9 +0,0 @@
-package io.micronaut.docs.ioc.mappers;
-
-import io.micronaut.core.annotation.Introspected;
-
-//tag::class[]
-@Introspected
-public record ContactForm(String firstName, String lastName) {
-}
-//end::class[]
